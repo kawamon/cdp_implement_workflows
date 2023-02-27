7a051156497da1f708ccc171b225945ebe0d2c54
Tatsuo Kawasaki <kawasaki@wwing.net>
2023-02-27 22:27:04 +0900

    updated sql
    remove *
    add td_client_id

--- access_log_processing/queries/11_process_orders.sql
+++ access_log_processing/queries/11_process_orders.sql
@@ -1,6 +1,7 @@
 SELECT
- *
- ,NORMALIZE(LOWER(email), NFKD) AS email_modified -- メールアドレスをノーマライズ、小文字にする
+ NORMALIZE(LOWER(email), NFKD) AS email_modified -- メールアドレスをノーマライズ、小文字にする
+ ,name
  ,SPLIT_PART(COALESCE(name, 'UNKNOWN UNKNOWN'), ' ', 1) AS fname -- name列を分割して名前を取り出す。NULLの場合はダミー文字列を埋める
- ,SPLIT_PART(COALESCE(name, 'UNKNOWN UNKNOWN'), ' ', 2) AS lname   -- name列を分割して名字を取り出す。NULLの場合はダミー文字列を埋める 
+ ,SPLIT_PART(COALESCE(name, 'UNKNOWN UNKNOWN'), ' ', 2) AS lname   -- name列を分割して名字を取り出す。NULLの場合はダミー文字列を埋める
+ ,td_client_id
 FROM l0_access_log.orders
